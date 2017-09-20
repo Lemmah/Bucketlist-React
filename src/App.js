e224ee849a3aa8fe9766f56b974602f822795ac2
Lemmah <lemayiannakolah@gmail.com>
2017-09-20 11:57:32 +0300

    ch(setup): Set Up Basic Page Structure

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
 import React, { Component } from 'react';
 import './App.css';
+import NavigationBar from './Components/NavigationBar'
+import PageLayout from './Components/PageLayout'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        BucketlistApp Rendering
-      </div>
-    );
-  }
+	constructor(){
+		super();
+		this.state = {
+			authenticated: false,
+		}
+	}
+
+	authenticate(){
+		return null;
+	}
+
+	render() {
+		return (
+		  <div className="App">
+		  	<NavigationBar 
+		  		authenticated={this.authenticate()}
+		  	/>
+		  	<PageLayout 
+		  		authenticated={this.authenticate()}
+		  	/>
+		  	{/*<Footer />*/}
+		  </div>
+		);
+	}
 }
 
 export default App;
