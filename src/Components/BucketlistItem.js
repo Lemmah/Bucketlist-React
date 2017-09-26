11156458f304eec442d502d42d3fd05e64032bb5
Lemmah <lemayiannakolah@gmail.com>
2017-09-26 07:29:18 +0300

    ch(bucketlists) Add Action Buttons

--- src/Components/BucketlistItem.js
+++ src/Components/BucketlistItem.js
@@ -1,36 +1,88 @@
 import React, { Component } from 'react'
+import { Button, ButtonToolbar, Row, Col } from 'react-bootstrap'
 
+const buttonsInstance = (
+  <ButtonToolbar>
+    <Button>A</Button>
+    <Button bsStyle="primary">B</Button>
+    <Button bsStyle="success">C</Button>
+    <Button bsStyle="info">D</Button>
+    <Button bsStyle="warning">E</Button>
+    <Button bsStyle="danger">F</Button>
+  </ButtonToolbar>
+);
 
 class BucketlistItem extends Component{
 
 	displayBucketlistItem(){
-		if(!this.props.bucketlistItems){
-			return(
+		if(!this.props.bucketlistName){
+			return (
 				<ul className="list-group">
+					<h3>
+							<center>
+								Welcome! Before you die...?
+							</center>
+						</h3>
 				  <li className="list-group-item"></li>
 				  <li className="list-group-item">
-				  	This Bucketlist is empty.
+				  	You currently don't have any bucketlists! Maybe you should
+				  	start by creating one then you items in it will appear here.
 				  </li>
 				  <li className="list-group-item"></li>
 				</ul>
 			);
 		} else {
-			return (
-				<ul className="list-group"> 
+			if(!this.props.bucketlistItems.length > 0){
+				return(
+					<ul className="list-group">
 						<h3>
 							<center>
 								{this.props.bucketlistName}
 							</center>
 						</h3>
-					{this.props.bucketlistItems.map((bucketlistItem) => {
-					   return (
-					   	<li
-					   			key={bucketlistItem.id}
-					   			className="list-group-item bucketlistItem"
-					   	>{bucketlistItem.name}</li>);
-					})}
-				</ul>
-			);
+					  <li className="list-group-item"></li>
+					  <li className="list-group-item">
+					  	There are no activities in here.
+					  </li>
+					  <li className="list-group-item"></li>
+					</ul>
+				);
+			} else {
+				return (
+					<ul className="list-group">
+						<Row>
+			      	<Col xs={6} md={6}>
+			      		<h3>
+									<center>
+										{this.props.bucketlistName}
+									</center>
+								</h3>
+			      	</Col>
+			      	<Col xs={6} md={6}>
+			      		{buttonsInstance}
+			      	</Col>
+		      	</Row> 
+						{this.props.bucketlistItems.map((bucketlistItem) => {
+						   return (
+						   	<li
+						   			key={bucketlistItem.id}
+						   			className="list-group-item bucketlistItem"
+						   	>
+						   	<Row>
+						   		<Col md={6} sm={6}>
+						   		{bucketlistItem.name}
+						   		</Col>
+						   		<Col md={6} sm={6} style={{textAlignment: "right"}}>{' '}
+						   		<Button bsStyle="success">A</Button>{' '}
+						   		<Button bsStyle="warning">E</Button>{' '}
+						   		<Button bsStyle="danger">D</Button>{' '}
+						   		</Col>
+						   	</Row>
+						   	</li>);
+						})}
+					</ul>
+				);
+			}
 		}
 	}
 
