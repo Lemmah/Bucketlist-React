e592befa41227f9cd8f3dca9b6816d1559167a28
Lemmah <lemayiannakolah@gmail.com>
2017-10-02 19:45:44 +0300

    ch(testing): Write tests for the Bucketlist Component

--- src/__tests__/Bucketlists/Bucketlist.test.js
+++ src/__tests__/Bucketlists/Bucketlist.test.js
@@ -1,10 +1,68 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import Bucketlist from '../../Components/Bucketlists/Bucketlist';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-15';
+configure({ adapter: new Adapter() });
+import { shallow, mount, render } from 'enzyme';
 
-it('Displays Bucketlist without Crashing', () => {
-	const rendered = renderer.create(
-		<Bucketlist />,
+
+describe("<Bucketlist />", () => {
+	const wrapper = shallow(
+		<Bucketlist 
+			createBucketlist={() => {}}
+			getAllBucketlistItems={() => {}}
+			bucketlists={[null, 'Lemmah']}
+		/>
 		);
-	expect(rendered.toJSON()).toMatchSnapshot();
+	const instance = wrapper.instance();
+	const empty = shallow(
+		<Bucketlist
+			bucketlists={null}
+		/>
+		);
+	const emptyInstance = empty.instance();
+
+	it('Displays Bucketlist without Crashing', () => {
+		const rendered = renderer.create(
+			<Bucketlist />,
+			);
+		expect(rendered.toJSON()).toMatchSnapshot();
+	})
+
+	it('Displays Bucketlist Items', () => {
+		instance.displayItems('Lemmah')
+	})
+
+	it('Conditionally Renders Bucketlists', () => {
+		instance.displayBucketlists();
+		emptyInstance.displayBucketlists();
+	})
+
+	it('Opens the Modal for an Action', () => {
+		instance.setState({
+			formShow: false,
+		});
+		instance.formOpen();
+		expect(instance.state.formShow).toBe(true);
+	})
+
+	it('Form Close actually closes the Modal,', () => {
+		instance.setState({
+			formShow: true,
+		});
+		instance.formClose();
+		expect(instance.state.formShow).toBe(false);
+	})
+
+	it('Closes the Modal form after Action', () => {
+		instance.setState({
+			formShow: true,
+		});
+		instance.createBucketlist('Lemmah');
+		expect(instance.state.formShow).toBe(false);
+	})
 })
+
+
+
