d6cad8f8e6e941b702d793dbc6e67db5814c6db3
Lemmah <lemayiannakolah@gmail.com>
2017-10-03 11:08:01 +0300

    ch(testing): Write tests for the PageLayout Component

--- src/__tests__/Commons/PageLayout.test.js
+++ src/__tests__/Commons/PageLayout.test.js
@@ -1,10 +1,107 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import PageLayout from '../../Components/Commons/PageLayout';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-15';
+configure({ adapter: new Adapter() });
+import { shallow, mount, render } from 'enzyme';
+
+const dummyCredentials = {
+	"email" : "jnlemayian@gmail.com",
+	"password": "easyPassword", 
+}
+const dummyAuthenticationInfo = {
+    "message": {
+        "access_token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1MDcwMDkyMTcsImlhdCI6MTUwNzAwNTYxNywic3ViIjoxfQ.idObEdx7BfAOeKGmh8w80wk-hx5J-JhUgWbiYDqb1fE",
+        "info": "You logged in successfully.",
+        "user": "jnlemayian@gmail.com"
+    }
+}
+
+const dummyProps = {
+	newBucketlist: null,
+	newBucketlistItem: null,
+	authenticated: {dummyAuthenticationInfo},
+	registrationError: null, 
+}
+
+const sampleBucketlists = [
+      {
+          "created_by": 1,
+          "date_created": "Mon, 02 Oct 2017 20:10:40 GMT",
+          "date_modified": "Mon, 02 Oct 2017 20:10:40 GMT",
+          "id": 138,
+          "name": "Lemmah"
+      },
+      {
+          "created_by": 1,
+          "date_created": "Mon, 02 Oct 2017 20:10:48 GMT",
+          "date_modified": "Mon, 02 Oct 2017 20:10:48 GMT",
+          "id": 139,
+          "name": "Lemmah22"
+      },
+      null
+  ]
+
+const sampleItems = [
+    {
+        "belongs_to": 138,
+        "date_created": "Mon, 02 Oct 2017 20:14:10 GMT",
+        "date_modified": "Mon, 02 Oct 2017 20:14:10 GMT",
+        "id": 119,
+        "name": "Lemmah22"
+    },
+    {
+        "belongs_to": 138,
+        "date_created": "Mon, 02 Oct 2017 20:14:17 GMT",
+        "date_modified": "Mon, 02 Oct 2017 20:14:17 GMT",
+        "id": 120,
+        "name": "Lemmah"
+    }
+]
 
 it('Displays PageLayout without Crashing', () => {
 	const rendered = renderer.create(
 		<PageLayout />,
 		);
 	expect(rendered.toJSON()).toMatchSnapshot();
+});
+
+describe('Shows Dashboard if logged in', () => {
+	const wrapper = mount(
+		<PageLayout
+	      newBucketlist={null}
+	      newBucketlistItem={null}
+	      authenticated={dummyAuthenticationInfo}
+	      onRegister={()=> {}}
+	      registrationError={()=> {}}
+	      onCreateBucketlist={()=> {}}
+	      onCreateBucketlistItem={()=> {}}
+	  />
+	);
+	const instance = wrapper.instance();
+	/* Set mounted state */
+	instance.setState({
+		token: dummyAuthenticationInfo.message.access_token,
+		user: dummyAuthenticationInfo.message.user,
+		bucketlists: sampleBucketlists,
+		items: sampleItems,
+		bucketlistOnFocus: sampleBucketlists[0],
+		firstFocus: false,
+	});
+
+	it('Registers new User using Credentials', () => {
+		instance.registerUser(dummyCredentials);
+	});
+
+	it('Deletes Bucketlist Item using Id', () => {
+		instance.deleteBucketlistItem(119);
+	});
+
+
+	it('Deletes Bucketlist using Id', () => {
+		instance.deleteBucketlist(139);
+	});
+
+
 })
